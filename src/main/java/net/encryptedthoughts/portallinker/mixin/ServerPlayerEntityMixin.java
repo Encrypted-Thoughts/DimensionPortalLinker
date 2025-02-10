0097d41f27f6a4420e460249c9bbff493448519a
Encrypted <thoughts.encrypted@gmail.com>
2025-02-09 19:22:24 -0600

    fix for player respawn logic when OverrideWorldSpawn enabled but OverridePlayerSpawn disabled so that if a player dies in a dimension without a spawn point set they'll go to the correct dimension world spawn

--- src/main/java/net/encryptedthoughts/portallinker/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/encryptedthoughts/portallinker/mixin/ServerPlayerEntityMixin.java
@@ -4,16 +4,21 @@ import net.encryptedthoughts.portallinker.util.WorldHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends Entity {
+    @Shadow @Nullable public abstract BlockPos getSpawnPointPosition();
+
     public ServerPlayerEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
@@ -23,7 +28,9 @@ public abstract class ServerPlayerEntityMixin extends Entity {
     {
         var info = WorldHelper.getDimensionInfo(getWorld().getRegistryKey().getValue().toString());
         var server = getServer();
-        if (info != null && info.OverridePlayerSpawn && info.SpawnDimension != null && server != null) {
+
+        var playerSpawnPoint = this.getSpawnPointPosition();
+        if (info != null && (info.OverridePlayerSpawn || (playerSpawnPoint == null && info.OverrideWorldSpawn)) && info.SpawnDimension != null && server != null) {
             var spawnPoint = info.getSpawnPoint();
             var world = WorldHelper.getWorldByName(server, info.SpawnDimension);
             if (spawnPoint != null && world != null)
