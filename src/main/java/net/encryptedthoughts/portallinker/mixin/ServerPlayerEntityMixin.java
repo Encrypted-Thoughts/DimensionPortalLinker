ce374fcb12342b7bb29f27032d32fa8ef91787ac
Encrypted <thoughts.encrypted@gmail.com>
2025-02-12 10:57:38 -0600

    changed how overriding spawns works so a secondary check for spawnable beds/respawn anchors isn't necessary. Still not sure if there's a more optimal solution though that'd be less likely to conflict with other mods accessing the same code.

--- src/main/java/net/encryptedthoughts/portallinker/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/encryptedthoughts/portallinker/mixin/ServerPlayerEntityMixin.java
@@ -1,22 +1,14 @@
 package net.encryptedthoughts.portallinker.mixin;
 
+import net.encryptedthoughts.portallinker.DimensionInfo;
 import net.encryptedthoughts.portallinker.util.WorldHelper;
-import net.minecraft.block.BedBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.RespawnAnchorBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,56 +16,42 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends Entity {
-
-    @Shadow @Nullable public abstract BlockPos getSpawnPointPosition();
-
-    @Shadow public abstract RegistryKey<World> getSpawnPointDimension();
-
-    @Shadow public abstract float getSpawnAngle();
-
-    @Shadow public abstract boolean isSpawnForced();
-
     public ServerPlayerEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
-    @Inject(method = "getRespawnTarget", at = @At(value = "RETURN"), cancellable = true)
-    public void portallinker_modifySpawnPointDimension(boolean alive, TeleportTarget.PostDimensionTransition postDimensionTransition, CallbackInfoReturnable<TeleportTarget> cir)
+    @Inject(method = "getRespawnTarget", at = @At(value = "RETURN", ordinal = 0), cancellable = true)
+    public void portallinker_modifyPlayerSpawn(boolean alive, TeleportTarget.PostDimensionTransition postDimensionTransition, CallbackInfoReturnable<TeleportTarget> cir)
     {
         var info = WorldHelper.getDimensionInfo(getWorld().getRegistryKey().getValue().toString());
+        if (info != null && info.OverridePlayerSpawn)
+            overrideSpawn(info, postDimensionTransition, cir);
+    }
+
+    @Inject(method = "getRespawnTarget", at = @At(value = "RETURN", ordinal = 1), cancellable = true)
+    public void portallinker_modifyFailedPlayerSpawn(boolean alive, TeleportTarget.PostDimensionTransition postDimensionTransition, CallbackInfoReturnable<TeleportTarget> cir)
+    {
+        var info = WorldHelper.getDimensionInfo(getWorld().getRegistryKey().getValue().toString());
+        if (info != null && info.OverrideWorldSpawn)
+            overrideSpawn(info, postDimensionTransition, cir);
+    }
+
+    @Inject(method = "getRespawnTarget", at = @At(value = "RETURN", ordinal = 2), cancellable = true)
+    public void portallinker_modifyWorldSpawn(boolean alive, TeleportTarget.PostDimensionTransition postDimensionTransition, CallbackInfoReturnable<TeleportTarget> cir)
+    {
+        var info = WorldHelper.getDimensionInfo(getWorld().getRegistryKey().getValue().toString());
+        if (info != null && info.OverrideWorldSpawn)
+            overrideSpawn(info, postDimensionTransition, cir);
+    }
+
+    @Unique
+    private void overrideSpawn(DimensionInfo info, TeleportTarget.PostDimensionTransition postDimensionTransition, CallbackInfoReturnable<TeleportTarget> cir) {
         var server = getServer();
-
-        var validPlayerSpawn = false;
-        var playerSpawnPoint = this.getSpawnPointPosition();
-        if (playerSpawnPoint != null && server != null) {
-            var serverWorld = server.getWorld(this.getSpawnPointDimension());
-            if (serverWorld != null)
-                validPlayerSpawn = isValidRespawnPosition(serverWorld, playerSpawnPoint, this.getSpawnAngle(), this.isSpawnForced());
-        }
-
-        if (info != null && (info.OverridePlayerSpawn || (!validPlayerSpawn && info.OverrideWorldSpawn)) && info.SpawnDimension != null && server != null) {
+        if (info.SpawnDimension != null && server != null) {
             var spawnPoint = info.getSpawnPoint();
             var world = WorldHelper.getWorldByName(server, info.SpawnDimension);
             if (spawnPoint != null && world != null)
                 cir.setReturnValue(new TeleportTarget(world, spawnPoint.toBottomCenterPos(), Vec3d.ZERO, 0.0F, 0.0F, postDimensionTransition));
-        }
-    }
-
-    @Unique
-    private static boolean isValidRespawnPosition(ServerWorld world, BlockPos pos, float spawnAngle, boolean spawnForced) {
-        BlockState blockState = world.getBlockState(pos);
-        Block block = blockState.getBlock();
-        if (block instanceof RespawnAnchorBlock && (spawnForced || blockState.get(RespawnAnchorBlock.CHARGES) > 0) && RespawnAnchorBlock.isNether(world)) {
-            return RespawnAnchorBlock.findRespawnPosition(EntityType.PLAYER, world, pos).isPresent();
-        } else if (block instanceof BedBlock && BedBlock.isBedWorking(world)) {
-            return BedBlock.findWakeUpPosition(EntityType.PLAYER, world, pos, blockState.get(BedBlock.FACING), spawnAngle).isPresent();
-        } else if (!spawnForced) {
-            return false;
-        } else {
-            boolean canMobSpawnInsideLegs = block.canMobSpawnInside(blockState);
-            BlockState headBlockState = world.getBlockState(pos.up());
-            boolean canMobSpawnInsideHead = headBlockState.getBlock().canMobSpawnInside(headBlockState);
-            return canMobSpawnInsideLegs && canMobSpawnInsideHead;
         }
     }
 }
